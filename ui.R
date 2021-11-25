d170cb483da1ff19d6a06bfd70513fe92c016eaf
cmilando <44262671+cmilando@users.noreply.github.com>
2021-11-25 06:56:28 -0700

    updated file names
    some errors with rsconnect

--- ui.R
+++ ui.R
@@ -17,7 +17,6 @@ library(shiny)
 library(tidyverse)
 library(nloptr)
 library("scales")
-library(shinybusy)
 library(ggpmisc)
 library(ggpubr)
 
@@ -337,7 +336,7 @@ ui <- fluidPage(
   h2("Loan payoff decision tool",align = 'center'),
   
   h4("Helping you make decisions about investing versus paying off loans.", 
-     a("Github", href="https://shiny.rstudio.com/articles/dynamic-ui.html"),
+     a("Github", href="https://github.com/cmilando/loan-payoff"),
      align = 'center'),
   
   # plot
@@ -448,57 +447,4 @@ ui <- fluidPage(
   )
 )
 
-# =============================================================================
-# Define reactive server logic
-server <- function(input, output, session) {
 
-  # what happens when the optimize button is clicked
-  observeEvent(input$optimize, {
-    show_modal_spinner(
-      text = "Running optimizer - see console",
-      spin = "fading-circle"
-    )
-
-    output <- payoff_optimizer(input)
-
-    remove_modal_spinner()
-
-    updateNumericInput(session, "p_revenue_direct", value = output[1])
-    updateNumericInput(session, "investment_skim_threshold", value = output[2])
-    updateNumericInput(session, "investment_skim_p", value = output[3])
-  })
-
-  # what happens when the reset button is clicked
-  observeEvent(input$reset, {
-    updateSliderInput(session, "p_revenue_direct", value = 0.5)
-    updateNumericInput(session, "investment_skim_threshold", value = 3000)
-    updateSliderInput(session, "investment_skim_p", value = 0.5)
-  })
-
-  # draws the plot
-  # because reactive, it updates each time
-  output$distPlot <- renderPlot({
-    my_loan_payoff(
-      x = c(
-        input$p_revenue_direct,
-        input$investment_skim_threshold,
-        input$investment_skim_p
-      ),
-      annual_interest_rate = input$annual_interest_rate,
-      annual_growth_rate = input$annual_growth_rate,
-      total_monthly_revenue = input$total_monthly_revenue,
-      asset_in_investments = input$asset_in_investments,
-      loan_to_payoff = input$loan_to_payoff,
-      capital_gains_tax = input$capital_gains_tax,
-      max_months = input$max_months,
-      pslf = input$pslf,
-      skim_check = input$skim_check,
-      make_plot = T
-    )
-  })
-}
-
-
-# =============================================================================
-# Run the application
-shinyApp(ui = ui, server = server)
